92eb9aa2856a9fb241d2cfa3e4a67665667dba75
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2021-11-17 20:42:57 -0700

    Added start quiz button

---
+++ Assets/JS/script.js
@@ -0,0 +1,32 @@
+//VARIABLES
+// const startButton = document.querySelector(".start_button");
+// const quizBox = document.getElementById("quiz");
+// const questions = [
+//     {
+//         question: 'What does HTML Mean?',
+//         answers: ['answer1', 'answer2', 'answer3'],
+//         correct: 'answer1',
+//     },
+//     {
+//         question: 'What does HTML Mean?',
+//         answers: ['answer1', 'answer2', 'answer3'],
+//         correct: 'answer1',
+//     },
+//     {
+//         question: 'What does HTML Mean?',
+//         answers: ['answer1', 'answer2', 'answer3'],
+//         correct: 'answer1',
+//     },
+// ]
+// //Function
+
+// function startQuiz(){
+//     //unhide card element
+//     quizBox.classList.remove("hide");
+//     //use innertext to render questions and answers to user interface
+// }
+
+
+// //Eventlistener section
+
+// startButton.addEventListener("click", startQuiz);
