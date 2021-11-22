432cd0f43c63777d275f69c4f3246eeb4093df75
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2021-11-22 12:05:54 -0700

    added function to display quiz box and hide info box when 'lets do this' button is clicked

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,12 +1,25 @@
 //VARIABLES (what variables do I need?)
 const startButton = document.querySelector(".start");
-const infoCard = document.querySelector(".info-card")
+const infoCard = document.querySelector(".info-card");
+const quiz = document.querySelector(".quiz-card");
+const noThanks = document.querySelector(".no-thanks");
+const letsDoThis = document.querySelector(".lets-do-this")
 
 function startQuiz(){
+//remove display none from info card
 infoCard.classList.remove("d-none");
 }
-
+//when start button is clicked, display info card
 startButton.addEventListener("click",startQuiz);
+
+function openQuiz(){
+//when "lets do this" button is clicked, it will hide the info box and display the quiz box.
+quiz.classList.remove("d-none");
+infoCard.classList.add("d-none");
+}
+letsDoThis.addEventListener("click",openQuiz)
+
+
 
 // const questions = [
 //     {
